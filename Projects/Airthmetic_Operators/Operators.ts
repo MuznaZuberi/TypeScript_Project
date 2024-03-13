ff57fbf79bfbbd03fd87792ecc2a700ae0b3e49f
Muzna Amir <muznazuberi123@gmail.com>
2024-03-12 23:36:56 -0700

    First Commit

---
+++ Projects/Airthmetic_Operators/Operators.ts
@@ -0,0 +1,37 @@
+//Airthmetic Operators
+
+//ADDITION
+let num1: number = 9;
+let num2: number = 7;
+let result = num1 + num2;
+console.log("Addition : " ,result);
+
+//SUBTRACTION
+let num3: number = 9;
+let num4: number = 7;
+let result1 = num3 - num4;
+console.log("Subtraction : " ,result1);
+
+//MULTIPLICATION
+let num5: number = 9;
+let num6: number = 2;
+let result2 = num5 * num6;
+console.log("Multiplication : " ,result2);
+
+//DIVIVSION
+let num7: number = 12;
+let num8: number = 4 ;
+let result3 = num7 / num8;
+console.log("Division : " ,result3);
+
+//EXPONENTIATION
+let num9 : number = 7;
+let result4 =  num9 ** 2;
+
+console.log("Exponentiation : " ,result4);
+
+//MODULUS
+let num10: number = 30;
+let num11: number = 7;
+let result5 = num10 % num11;
+console.log("Modulus : " ,result5);
---
+++ Projects/Unary_Operators/UnaryOperators.ts
@@ -0,0 +1,15 @@
+//UNARY OPERATORS
+
+
+//INCREMENT
+let a: number = 70;
+let result = ++a;
+
+console.log("Increment :", result);
+
+
+//DECREMENT
+let b: number = 91;
+let another_result = --b;
+
+console.log("Decrement :" , another_result);
